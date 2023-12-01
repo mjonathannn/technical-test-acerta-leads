8b4643dbaabf3fa5506b69c8b022441964fbbf1a
Michael Jonathan <michaeljonathan_98@hotmail.com>
2023-12-01 18:19:39 -0300

    refac: header refactor

---
+++ src/components/header/Header.tsx
@@ -0,0 +1,41 @@
+import { useNavigate } from "react-router-dom"
+
+import { HeaderContainer, NavContainer } from "./HeaderStyles"
+
+import AcertaLogo from "../../images/acerta_logo.svg"
+import { LabelButton } from ".."
+
+type Props = {
+  label: string
+  showButton?: boolean
+}
+
+const Header = ({ label, showButton }: Props): JSX.Element => {
+  const navigate = useNavigate()
+
+  const handleClick = () => {
+    navigate("/createLeads")
+  }
+
+  return (
+    <HeaderContainer>
+      <img src={AcertaLogo} alt="Acerta Logo" title="Acerta Logo" />
+
+      <NavContainer>
+        <h1>{label}</h1>
+
+        {showButton && (
+          <LabelButton
+            type="button"
+            appearance="Secondary"
+            label="Novo lead"
+            showIcon
+            onClick={() => handleClick()}
+          />
+        )}
+      </NavContainer>
+    </HeaderContainer>
+  )
+}
+
+export default Header
---
+++ src/components/header/HeaderStyles.ts
@@ -0,0 +1,21 @@
+import styled from "styled-components"
+
+export const HeaderContainer = styled.header`
+  margin: 32px 0;
+`
+export const NavContainer = styled.nav`
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+  height: 40px;
+  margin: 32px 0 0 0;
+
+  h1 {
+    margin: 0;
+    color: var(--grey-darker);
+    font-size: 24px;
+    font-style: normal;
+    font-weight: 600;
+    line-height: 120%;
+  }
+`
